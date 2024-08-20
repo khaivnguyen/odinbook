96f506f4eea222ad9a1d8a4410a1d22a6609d32a
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-20 13:09:58 +0700

    Initialize PostsController and its view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create 
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
