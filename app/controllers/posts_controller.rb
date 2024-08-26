3ec7752b4ae493763c035026391782d74fd924f8
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-26 10:05:11 +0700

    fix typo in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    followed_user_ids = current_user.following.pluck(:id)
+    followed_user_ids = current_user.followings.pluck(:id)
     @posts = Post.where(user_id: [current_user.id] + followed_user_ids).order(created_at: :desc)
   end
 
